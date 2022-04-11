25571c8e43504b1c50c468b2d93a5142f0dd870b
Juhwan <dev.juhwanlee@gmail.com>
2022-04-11 19:59:48 +0900

    Add function: header logo useNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import 'nes.css/css/nes.min.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/UI/Header';
 import InputWord from './components/UI/InputWord';
@@ -8,7 +8,6 @@ import Home from './components/pages/Home';
 import NotFound from './components/pages/NotFound';
 
 function App() {
-  // const history = useHistory();
   return (
     <>
       <Header />
